9c6c158f5a6c9536f2cb641688deb0381803acc8
Matías Batista <matibat@gmail.com>
2018-03-26 20:21:57 -0300

    Fix directory structure

---
+++ tarea1p2/runtests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+FLAG="$1"
+
+echo "\nIniciando..."
+NUMTEST="1"
+while [ "${NUMTEST}" -lt "14" ]; do
+  if [ "${NUMTEST}" -lt "10" ]; then NUMTEST="0${NUMTEST}"; fi
+
+  ./principal < test/${NUMTEST}.in > test/${NUMTEST}.sal
+  diff test/${NUMTEST}.out test/${NUMTEST}.sal
+  
+  if [ "$?" != "0" ]; then
+    echo "Error en el test #${NUMTEST}"
+    echo "\nEntrada:"
+    cat -n test/${NUMTEST}.in
+    if [ "x${FLAG}" != "x-a" ]; then
+      break
+    fi
+  fi
+  
+  NUMTEST="$(expr "$NUMTEST" "+" "1")"
+done
+echo "Fin"
